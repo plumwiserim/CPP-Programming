becec5defe61a9d7092891e9a1f1b37db0849997
LEE <dulfla_5@naver.com>
2021-09-07 12:44:16 +0900

    15 namespace 1

---
+++ 15_namespace_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+namespace Computer {
+	class Monitor {};
+	class Mouse {};
+	class MainMemory {};
+	class HardDisk {};
+}
+namespace Money {
+	class Won {};
+	class Dollar {};
+	class Franc {};
+}
+namespace Math {
+	class Log {};
+	class Exp {};
+	class Min {};
+	class Max {};
+}
+namespace Exception {
+	class RangeError {};
+	class AllocError {};
+	class NoSocketError {};
+}
+namespace Data_Structure {
+	class List {};
+	class Stack {};
+	class Queue {};
+	class HashTable {};
+}
---
+++ 15_namespace_1.md
@@ -0,0 +1,14 @@
+문제
+
+determine namespaces from a collection of classes below
+
+Monitor, Mouse, Won, Dollar, MainMemory, Franc, HardDisk, Log, Exp, Min, Max, RangeError, AllocError, NoSocketError, List, Stack, Queue, HashTable
+
+​
+
+실행 예시
+```
+0 Namespace1 - class1, class2, ...
+0 Namespace2 - class2, class2, ...
+0 ...
+```
